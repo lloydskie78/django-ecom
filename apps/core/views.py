be30ad4ee7bb2688f701bc6a7e8eb6ce1056e280
$amalBoii <49010379+lloydskie78@users.noreply.github.com>
2021-01-06 14:31:39 +0800

    Initial commit

---
+++ apps/core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from apps.store.models import Product
+
+
+def frontpage(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "frontpage.html", context)
+
+
+def contact(request):
+    return render(request, "contact.html")
